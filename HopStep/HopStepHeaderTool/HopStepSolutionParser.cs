c23bb0e2345006bdb6e3cc30a8e2fc7e4bec6a99
Keunwon Lee <highfence@naver.com>
2022-05-04 23:18:57 +0900

    Add ToolTest

--- HopStep/HopStepHeaderTool/HopStepSolutionParser.cs
+++ HopStep/HopStepHeaderTool/HopStepSolutionParser.cs
@@ -1,11 +1,30 @@
-﻿namespace HopStepHeaderTool
+﻿using System;
+using System.IO;
+
+namespace HopStepHeaderTool
 {
 	internal class HopStepSolutionParser : ISolutionParser
 	{
 		public SolutionSchema SolutionSchema => throw new System.NotImplementedException();
 		public void Parse(string path)
 		{
-			throw new System.NotImplementedException();
+			try
+            {
+				var fullPath = Path.GetFullPath(path);
+				Console.WriteLine($"Solution path : {fullPath}");
+
+				var dirInfo = new DirectoryInfo(fullPath);
+				var headerFiles = dirInfo.GetFiles("*.h");
+
+				foreach (var headerFile in headerFiles)
+                {
+					Console.WriteLine(headerFile.FullName);
+                }
+            }
+            catch
+            {
+				throw new NotImplementedException();
+            }
 		}
 	}
 }
