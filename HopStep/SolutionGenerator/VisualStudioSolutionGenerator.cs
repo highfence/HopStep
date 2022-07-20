db9146aef3b77bd2b0e5f81e764dfdf7a753ab87
근원 이 <Highfence@naver.com>
2022-07-20 19:18:34 +0900

    Making solution schema info flat, not recursive

--- HopStep/SolutionGenerator/VisualStudioSolutionGenerator.cs
+++ HopStep/SolutionGenerator/VisualStudioSolutionGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
 
 namespace SolutionGenerator
@@ -22,23 +23,57 @@ namespace SolutionGenerator
 
         public SolutionFilterSchema FilterSchema { get; set; }
 
+        struct SolutionFilterInfo
+		{
+            public List<string> Filters;
+            public List<string> HeaderInclude;
+            public List<string> CppInclude;
+		}
+
+        private SolutionFilterInfo _flatFilterInfo;
+
         public void Generate()
         {
             if (string.IsNullOrEmpty(SolutionRoot)) return;
 
-            GatherSolutionFilter();
+            GatherFilterSchemaInfo();
+
+            _flatFilterInfo = new SolutionFilterInfo 
+            {
+                Filters = new List<string>(),
+                HeaderInclude = new List<string>(),
+                CppInclude = new List<string>()
+            };
+
+            MakeSolutionFilterInfo(FilterSchema, string.Empty);
 
             ModifyProjectFile();
             ModifyFilterFile();
         }
 
-        private void GatherSolutionFilter()
+        private void GatherFilterSchemaInfo()
         {
             var gatherer = new DirectoryInfoGatherer(SolutionRoot);
             gatherer.Gather();
 
             FilterSchema = gatherer.RootSchema;
+            FilterSchema.FilterName = string.Empty;
         }
+
+        private void MakeSolutionFilterInfo(SolutionFilterSchema currentDirectorySchema, string previousDirectory)
+		{
+            var currentDirectory = Path.Combine(previousDirectory, currentDirectorySchema.FilterName);
+            if (string.IsNullOrEmpty(currentDirectory) == false)
+			{
+                _flatFilterInfo.Filters.Add(currentDirectory);
+                currentDirectory += "\\";
+			}
+
+            _flatFilterInfo.HeaderInclude.AddRange(currentDirectorySchema.HeaderFileNames.Select(headerName => $"{currentDirectory}{headerName}"));
+            _flatFilterInfo.CppInclude.AddRange(currentDirectorySchema.CppFileNames.Select(cppName => $"{currentDirectory}{cppName}"));
+
+            currentDirectorySchema.Childs.ForEach(child => MakeSolutionFilterInfo(child, currentDirectory));
+		}
 
         private void ModifyProjectFile()
         {
@@ -151,7 +186,11 @@ namespace SolutionGenerator
             }
 
             XmlNode filterMetaGroup = itemGroupNodes[0];  
-            
         }
+
+        private void AppendFilterMetaInfoRecursive(XmlDocument xmlDoc, XmlNode xmlNode, SolutionFilterSchema schema, string baseDirectory)
+		{
+            
+		}
     }
 }
