de7f19814d7b35d96601e30a52822c2d1cd83d15
Keunwon Lee <highfence@naver.com>
2022-10-19 14:00:55 +0900

    Implementing HFunction...

--- HopStep/HopStepEngine/Core/Container/VariadicStack.h
+++ HopStep/HopStepEngine/Core/Container/VariadicStack.h
@@ -23,6 +23,18 @@ namespace HopStep::Container
 		void Push(uint8* InData, int16 DataSize);
 		void Pop(uint8* OutData, int16 DataSize);
 
+		void PushReference(uint8*& InData);
+		void PopReference(uint8*& OutData);
+
+		template <typename TType>
+		void PushByType(const TType& InValue);
+
+		template <typename TType>
+		TType PopByType();
+
+		uint16 GetTopOffset() const { return CurrentOffset; }
+		const uint8* GetData() const { return &Storage[0]; }
+
 	protected:
 
 		uint8 Storage[StackSize];
@@ -49,5 +61,39 @@ namespace HopStep::Container
 		CurrentOffset -= DataSize;
 		--Count;
 		HGenericMemory::MemCpy(OutData, &Storage[CurrentOffset], DataSize);
+	}
+
+	template<int32 StackSize>
+	inline void HVariadicStack<StackSize>::PushReference(uint8*& InData)
+	{
+		int16 DataSize = sizeof(uint8*);
+		HCheck(CurrentOffset + DataSize <= StackSize);
+
+		std::uintptr_t CastedData = (uintptr_t)InData;
+		Push((uint8*)CastedData, DataSize);
+	}
+
+	template<int32 StackSize>
+	inline void HVariadicStack<StackSize>::PopReference(uint8*& OutData)
+	{
+		uintptr_t CastedData;
+		Pop((uint8*)&CastedData, sizeof(uintptr_t));
+		OutData = (uint8*)CastedData;
+	}
+
+	template<int32 StackSize>
+	template<typename TType>
+	inline void HVariadicStack<StackSize>::PushByType(const TType& InValue)
+	{
+		Push((uint8*)&InValue, sizeof(TType));
+	}
+
+	template<int32 StackSize>
+	template<typename TType>
+	inline TType HVariadicStack<StackSize>::PopByType()
+	{
+		TType Result{};
+		Pop((uint8*)&Result, sizeof(TType));
+		return Result;
 	}
 }
--- HopStep/HopStepEngine/CoreObject/Reflection/Function.h
+++ HopStep/HopStepEngine/CoreObject/Reflection/Function.h
@@ -1,10 +1,44 @@
 #pragma once
 #include "Struct.h"
+#include "Core\Container\VariadicStack.h"
 
 namespace HopStep::CoreObject::Reflection
 {
-	/**
-	 * 
+	using HVariadicStack = HopStep::Container::HVariadicStack<256>;
+	/*
+	 */
+	struct HDirectionFunctionCallFrame : public HVariadicStack
+	{
+		HDirectionFunctionCallFrame() 
+			: HVariadicStack()
+			, OutputOffset(0)
+			, OutputSize(0) 
+		{}
+
+		void PushOutput(uint8* Output, int16 Size)
+		{
+			int16 Offset = GetTopOffset();
+			Push(Output, Size);
+			OutputOffset = Offset;
+			OutputSize = Size;
+		}
+
+		template <typename TOutType>
+		TOutType PopOutput()
+		{
+			HCheck(OutputSize == sizeof(TOutType));
+			TOutType Output;
+			Pop((uint8*)&Output, OutputSize);
+			return Output;
+		}
+
+	protected:
+
+		int16 OutputOffset;
+		int16 OutputSize;
+	};
+
+	/*
 	 */
 	class HFunction : public HStruct
 	{
@@ -17,15 +51,58 @@ namespace HopStep::CoreObject::Reflection
 
 		virtual ~HFunction() {};
 
-	private :
+		template <typename TReturnType, typename... TParamArgs>
+		TReturnType Invoke(void* InClassPtr, TParamArgs&&... Args);
+
+	protected:
+
+		virtual void InvokeImpl(HDirectionFunctionCallFrame& InvokeFrame) abstract;
+
+		template <typename... TParamArgs>
+		void SetParameters(HDirectionFunctionCallFrame& RefFrame, TParamArgs&&... Parameters);
+
+		template <typename TParamType>
+		int16 PushParamter(HDirectionFunctionCallFrame& RefFrame, TParamType&& Parameter);
 
 		const HStruct* const Owner;
 
 	};
 
+	/*
+	 */
 	class HNativeFunction : public HFunction
 	{
 	public:
 
+	protected:
+
+		virtual void InvokeImpl(HDirectionFunctionCallFrame& InvokeFrame) override;
 	};
+
+	template<typename TReturnType, typename ...TParamArgs>
+	inline TReturnType HFunction::Invoke(void* InClassPtr, TParamArgs && ...Args)
+	{
+		HDirectionFunctionCallFrame Frame;
+
+		uint8* ClassReference = (uint8*)InClassPtr;
+		Frame.PushReference(ClassReference);
+
+		SetParameters(Frame, Args);
+
+		return Frame.PopByType
+	}
+
+	template<typename... TParamArgs>
+	void HFunction::SetParameters(HDirectionFunctionCallFrame& RefFrame, TParamArgs&&... Parameter)
+	{
+		auto _ = { PushParamter(RefFrame, Parameter)... };
+	}
+
+	template <typename TParamType>
+	int16 HFunction::PushParamter(HDirectionFunctionCallFrame& RefFrame, TParamType&& Parameter)
+	{
+		RefFrame.PushByType<TParamType>(Parameter);
+		return 0;
+	}
+
 }
