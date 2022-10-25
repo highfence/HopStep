168ac58be3ceb4b2ad20ea37d012b3e67b56ce88
kwen won lee <highfence@naver.com>
2022-10-25 22:59:44 +0900

    Making AddNativeFunctionProperty method...

--- HopStep/HopStepEngine/CoreObject/Reflection/StructBuilder.cpp
+++ HopStep/HopStepEngine/CoreObject/Reflection/StructBuilder.cpp
@@ -1,5 +1,13 @@
 #include "HopStep.h"
+#include "StructBuilder.h"
 
 namespace HopStep::CoreObject::Reflection
 {
+	void HStructBuilder::AddNativeFunctionProperty(HStruct* InOwnerStruct, HNativeFuncPtr FuncPtr, HString FunctionName)
+	{
+		HUniquePtr<HProperty> NewProperty = std::make_unique<HNativeFunction>(FunctionName, InOwnerStruct, FuncPtr);
+
+		IntializePropertyFlags<HNativeFunction>(NewProperty.get());
+		InStruct->Properties.push_back(std::move(NewProperty));
+	}
 }
