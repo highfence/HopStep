f74dbe703943c408e4911d2484f547984e5412cb
kwen won lee <highfence@naver.com>
2022-11-05 20:26:32 +0900

    Complete Internal::DetectTrailingDigitFromString Method Test

--- HopStep/HopStepTest/CoreObjectTest.cpp
+++ HopStep/HopStepTest/CoreObjectTest.cpp
@@ -13,6 +13,7 @@ namespace HopStepTest
 		
 		TEST_METHOD(HName_DetectTrailingDigitFromStringTest)
 		{
+			// No digit string test
 			{
 				HopStep::HString NoDigitString(TEXT("NoDigitString"));
 				uint32 MustBeNoDigit = 0;
@@ -22,6 +23,54 @@ namespace HopStepTest
 
 				Assert::AreEqual((uint32)0, MustBeNoDigit);
 				Assert::AreEqual((uint32)0, MustBeNoLength);
+			}
+
+			// One digit string test
+			{
+				HopStep::HString OneDigitString(TEXT("OnDigitString9"));
+				uint32 Digit = 0;
+				uint32 Length = 0;
+
+				HopStep::Internal::DetectTrailingDigitFromString(OneDigitString, Digit, Length);
+
+				Assert::AreEqual((uint32)9, Digit);
+				Assert::AreEqual((uint32)1, Length);
+			}
+
+			// Multiple digit string test
+			{
+				HopStep::HString MultipleDigitString(TEXT("MultipleDigitString49783"));
+				uint32 Digit = 0;
+				uint32 Length = 0;
+
+				HopStep::Internal::DetectTrailingDigitFromString(MultipleDigitString, Digit, Length);
+
+				Assert::AreEqual((uint32)49783, Digit);
+				Assert::AreEqual((uint32)5, Length);
+			}
+
+			// Minus digit string test
+			{
+				HopStep::HString MinusDigitString(TEXT("MinusDigitString-883"));
+				uint32 Digit = 0;
+				uint32 Length = 0;
+
+				HopStep::Internal::DetectTrailingDigitFromString(MinusDigitString, Digit, Length);
+
+				Assert::AreEqual((uint32)883, Digit);
+				Assert::AreEqual((uint32)3, Length);
+			}
+
+			// Digit inside string test
+			{
+				HopStep::HString DigitInsideString(TEXT("Digit11299Inside97463String27649473"));
+				uint32 Digit = 0;
+				uint32 Length = 0;
+
+				HopStep::Internal::DetectTrailingDigitFromString(DigitInsideString, Digit, Length);
+
+				Assert::AreEqual((uint32)27649473, Digit);
+				Assert::AreEqual((uint32)8, Length);
 			}
 		}
 	};
