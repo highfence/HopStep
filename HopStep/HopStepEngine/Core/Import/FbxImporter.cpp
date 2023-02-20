fc39f2417a9aec5f12aca26075878ec3b3f2d04d
근원 이 <Highfence@naver.com>
2023-02-20 12:58:19 +0900

    Start using assimp library

--- HopStep/HopStepEngine/Core/Import/FbxImporter.cpp
+++ HopStep/HopStepEngine/Core/Import/FbxImporter.cpp
@@ -1,11 +1,11 @@
 #include "FbxImporter.h"
+#include "ThirdParty/Assimp/include/assimp/cimport.h"
 
 namespace HopStep
 {
 	bool HFbxImporter::OpenFile(const HString& Filename)
 	{
-		
-
+		auto _ = aiImportFile("", 0);
 		return false;
 	}
 }
