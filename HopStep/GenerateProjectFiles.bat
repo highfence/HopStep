1499a4b23d5597f686b187c3a6a58725936a9717
근원 이 <Highfence@bluehole.net>
2024-01-22 17:05:29 +0900

    Make msbuild log silent

--- HopStep/GenerateProjectFiles.bat
+++ HopStep/GenerateProjectFiles.bat
@@ -4,8 +4,8 @@ set BAT_DIR=%~dp0
 echo Current Batch Directory: %BAT_DIR%
 echo MsBuild.exe: %MS_BUILD%
 
-%MS_BUILD% /t:Rebuild /p:Configuration=Release %~dp0\HopStepHeaderTool\HopStepHeaderTool.csproj
-%MS_BUILD% /t:Rebuild /p:Configuration=Release %~dp0\SolutionGenerator\SolutionGenerator.csproj
+%MS_BUILD% /t:Rebuild /p:Configuration=Release %~dp0\HopStepHeaderTool\HopStepHeaderTool.csproj >nul 2>&1
+%MS_BUILD% /t:Rebuild /p:Configuration=Release %~dp0\SolutionGenerator\SolutionGenerator.csproj >nul 2>&1
 
 start /B /WAIT %~dp0HopStepHeaderTool\bin\Release\netcoreapp3.1\HopStepHeaderTool.exe Solution=%~dp0HopStepEngine\ Intermediate=%~dp0HopStepEngine\Intermediate\
 start /B /WAIT %~dp0SolutionGenerator\bin\Release\netcoreapp3.1\SolutionGenerator.exe %~dp0HopStepEngine\
